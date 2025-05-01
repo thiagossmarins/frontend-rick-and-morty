6db7259514427b3a245c7605ee3d455d92072d20
thiagossmarins <thiagodesouzasallesmarins@gmail.com>
2025-05-01 18:04:13 -0300

    Applying responsive in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,23 @@
 import { Header } from '../../components/Header'
 import { Search } from '../../components/Search'
+import background from '../../assets/img/background-rick-and-morty.png'
 import './index.css'
 
 export default function App() {
   return (
     <>
-      <Header />
+      <div className='background'>
+        <img 
+          src={background}
+          alt='Background Rick And Morty'
+        />
+      </div>
 
-      <Search />
+      <div className='content'>
+        <Header />
+
+        <Search />
+      </div>
     </>
   )
 }
