c8ca46cb2e0aac667feca8df7c08e533aff40052
dev-thiago <dev-thiago@Mac-Pro-de-dev-thiago.local>
2025-05-22 19:25:04 -0300

    Applying correct box-shadow

--- src/components/Cards/style.ts
+++ src/components/Cards/style.ts
@@ -32,7 +32,7 @@ export const Card = styled.div`
   &:hover {
     border: 2px solid #CBD736;
 
-        box-shadow:
+    box-shadow:
       0px 0px 5px 4px #C9D636,
       0px 0px 0px 3px #C9D636,
       0px 0px 0px 1px #00000099 inset;
